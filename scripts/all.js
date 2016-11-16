1e01b5200a5210ac2882636595f113f1f55d7246
Laura Brandt <laurambrandt@gmail.com>
2016-11-16 02:19:15 +0000

    some minor changes

--- scripts/all.js
+++ scripts/all.js
@@ -1,3 +1,19 @@
 $(document).ready(function() {
-    //alert("ready");
+    // keep navbar items the same width even when hovering
+    var widths = {};
+    $(".nav li").each(function(i) {
+        var w = $(this).innerWidth();
+        widths[i] = w;
+    });
+    
+    $(".nav li").hover(
+        function() {
+            var index = $(this).index(".nav li");
+            $(this).width(widths[index]);
+        },
+        function() {
+            var index = $(this).index(".nav li");
+            $(this).width(widths[index]);
+            $(this).children("a").css({ "padding-left": "0", "padding-right": "0" });
+        });
 });
